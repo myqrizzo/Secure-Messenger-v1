f89d110b0ab1a8b3438759cc29d30095c75af483
M <mdrizzzoh@gmail.com>
2020-04-22 23:55:48 -0400

    make it better

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const db = require('./config/keys').mongoURI;
 
 //Connect to Mongo
 mongoose
-   .connect(db, { useNewUrlParser: true, useUnifiedTopology: false } )
+   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true } )
    .then(() => console.log("MongoDB Connected..."))
    .catch(err => console.log(err));
 

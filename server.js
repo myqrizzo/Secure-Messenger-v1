2ecd8b7f027f4a4582d75e70e6549c6ef3cbf85b
M <mdrizzzoh@gmail.com>
2020-04-22 23:19:15 -0400

    make it better

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ app.use('/api/items', items);
 // Serve static assests if in production
 if(process.env.NODE_ENV === 'production'){
   //Set static folder
-  app.use(express.static(path.join(__dirname , 'build')));
+  app.use(express.static(path.resolve(__dirname, 'client')));
 
   app.get('*', (req, res) => {
-     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 

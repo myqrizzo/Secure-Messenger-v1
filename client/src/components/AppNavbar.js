8e440ea9ae345c33256aac5f943918133ee6947c
maltchev <maltchev@gmail.com>
2020-04-24 12:44:15 -0700

    change app name in AppNavbar

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -59,7 +59,7 @@ class AppNavbar extends Component {
             <div>
                 <Navbar color="dark" expand="sm" className="mb-5">
                     <Container>
-                        <NavbarBrand href="/">ShoppingList</NavbarBrand>
+                        <NavbarBrand href="/">Secure Messenger</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav className="ml-auto" navbar>

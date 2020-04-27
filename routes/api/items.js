1b2158fac98cd4d024e3fb2ea42350199e462af4
maltchev <maltchev@gmail.com>
2020-04-27 00:21:52 -0700

    trying to fix from email

--- routes/api/items.js
+++ routes/api/items.js
@@ -22,7 +22,7 @@ router.post('/', auth, (req, res) => {
     const newItem = new Item({
         body: req.body.body,
         toemail: req.body.toemail,
-        fromemail: req.user
+        fromemail: req.user.id
     });
 
     newItem.save().then(item => res.json(item));
